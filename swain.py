64660984494fcda4a7f05571adbe367029e11a38
mteam88 <matthew8edelen@gmail.com>
2022-01-10 16:43:24 +0000

    swain complete

--- swain.py
+++ swain.py
@@ -3,6 +3,7 @@ from pdfminer.pdfpage import PDFPage
 from pdfminer.pdfinterp import PDFResourceManager
 from pdfminer.pdfinterp import PDFPageInterpreter
 from pdfminer.converter import PDFPageAggregator
+import sys
 
 fp = open('Ch10.pdf', 'rb')
 rsrcmgr = PDFResourceManager()
@@ -10,15 +11,17 @@ laparams = LAParams()
 device = PDFPageAggregator(rsrcmgr, laparams=laparams)
 interpreter = PDFPageInterpreter(rsrcmgr, device)
 pages = PDFPage.get_pages(fp)
+sys.stdout = open('log.txt', 'w')
 
 for page in pages:
-    print('Processing next page...')
+    #print('Processing next page...')
     interpreter.process_page(page)
     layout = device.get_result()
     for lobj in layout:
         if isinstance(lobj, LTTextBox):
             x, y, text = lobj.bbox[0], lobj.bbox[3], lobj.get_text()
             if x == 138.0 or x == 162.0 or x == 162.5:
-                print("BING BING BING")
-                print('At %r is text: %s' % ((x, y), text))
+                #print("BING BING BING")
+                print('<p><strong>' + text + '</strong></p><p>&nbsp;</p>')
     #input()
+sys.stdout.close()
